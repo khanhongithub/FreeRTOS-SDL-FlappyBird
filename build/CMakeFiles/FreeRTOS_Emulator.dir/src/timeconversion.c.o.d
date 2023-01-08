31daf63ccbd8c5b5ecb6a06818d0b2641ce14847
Nino-tum <nino.obradovic@tum.de>
2023-01-08 02:12:08 +0100

    cleanup of temp files

--- build/CMakeFiles/FreeRTOS_Emulator.dir/src/timeconversion.c.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/FreeRTOS_Emulator.dir/src/timeconversion.c.o: \
- /home/baseduser/ESPL/FreeRTOS-Emulator/src/timeconversion.c \
- /usr/include/stdc-predef.h \
- /home/baseduser/ESPL/FreeRTOS-Emulator/include/timeconversion.h
--- build/CMakeFiles/commit.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/commit"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/commit.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
